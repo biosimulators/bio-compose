7d4d14e6c55bce3a19da0e258950d923ee99c5f9
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-08-28 14:08:13 -0400

    updates to html

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../bio_compose'))
+
+
 project = 'BioCompose'
 copyright = '2024, Alexander A. Patrie'
 author = 'Alexander A. Patrie'
