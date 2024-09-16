72d0449bbf9c7b932d8124e6fedfe16c40c59fb4
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-09-16 10:43:22 -0400

    updates to version

--- bio_compose/__init__.py
+++ bio_compose/__init__.py
@@ -1,9 +1,11 @@
 import os
-
 import toml
 
 from bio_compose.api import *
 
 
-# pyproject_file_path = os.path.join(os.path.dirname(__file__), '..', 'pyproject.toml')
-# __version__ = toml.load(pyproject_file_path)['tool']['poetry']['version']
+pyproject_file_path = os.path.join(os.path.dirname(__file__), '..', 'pyproject.toml')
+try:
+    __version__ = toml.load(pyproject_file_path)['tool']['poetry']['version']
+except:
+    __version__ = ' '
